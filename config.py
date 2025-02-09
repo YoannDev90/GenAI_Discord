5577562d03d7a55d23a1dad35e7ff3fd10856d9b
Yoann Dumont <yoanndev@outlook.fr>
2025-02-09 16:29:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 import json
 
-if not os.path.exists('config_alphallm.json'):
+if not os.path.exists('config.json'):
     TOKEN = input("Veuillez entrer le token du bot : ")
     OPENROUTER_API_KEY = input("Veuillez entrer la clé API OpenRouter : ")
-    with open('config_alphallm.json', 'w') as f:
+    with open('config.json', 'w') as f:
         json.dump({
             "token": TOKEN,
             "openrouter_api_key": OPENROUTER_API_KEY,
         }, f)
 else:
-    with open('config_alphallm.json', 'r') as f:
+    with open('config.json', 'r') as f:
         config = json.load(f)
         TOKEN = config['token']
         OPENROUTER_API_KEY = config['openrouter_api_key']
